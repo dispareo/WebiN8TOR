f924cb434604f87746c1f8f67b83d7ac818cdf34
dispareo <67999742+dispareo@users.noreply.github.com>
2021-07-21 17:17:25 -0500

    Update enumWeb.py

--- enumWeb.py
+++ enumWeb.py
@@ -7,18 +7,24 @@
 import subprocesses
 import sys
 import socket
+import pyfiglet
 
-host = sys.argv[1]
-ports = [80,443,8000,8080]
+banner = pyfiglet.figlet_format("WebiN8TOR")
+print(banner)
 
 if len(sys.argv) !=2:
   raise ValueError("Please provide an IP to target")
   
+host = sys.argv[1]
+ports = [80,443,8000,8080]
+  
 print("Starting enumeration... \n\nPreparing World Domination... \n\n Or maybe just a lil' pwnage")
 
-for x in ports:
-  s = s.socket.socket(socket(AF_INET, socket.SOCK_STREAM)
+
+try:
+  for x in ports:
+  s = s.socket.socket(socket_AF_INET, socket.SOCK_STREAM)
   result = s.connect_ex((host,x))
-    if result == 0:
-                      print(f"Port {x} is open, we'll enumerate that...")
-  
+    if result ==0:
+            print("Port {} is open, let's enumerate further".format(x))
+        s.close()  
